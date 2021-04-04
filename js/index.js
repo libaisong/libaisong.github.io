fdf569594429f36c208d92b44dfd13a49dba1c63
Li Baisong <baisong.li@foxmail.com>
2021-04-04 10:28:54 +0800

    增加js，对文章进行编号，并将"回到顶部"按钮合并成一个

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+    var subitems = document.querySelectorAll('.sub_item');
+    for(var i=0;i<subitems.length;i++)
+    {
+        var text = subitems[i].querySelector('a').querySelector('h1').innerHTML;
+        // console.log(text);
+        subitems[i].querySelector('a').querySelector('h1').innerHTML += "<span class='titlecount'>("+(subitems.length-i)+'/'+(subitems.length)+")</span>";
+    }
+
+    document.onscroll = function(){start()};
+    window.onresize = function(){start()};
+
+    function start()
+    {
+        // console.log('body'+document.body.clientWidth);
+        var computedStyle = getComputedStyle(document.body, null);
+        // console.log('left'+computedStyle.marginRight);
+
+        // console.log(window.pageYOffset);
+        var boot = document.querySelector('.backtoTop');
+        // console.log(boot);
+        if(window.pageYOffset < 10)
+        {boot.style.display = 'none';}
+        else
+        {boot.style.display = 'block';
+        boot.style.right = parseFloat(computedStyle.marginRight)+20+'px';
+        }
+
+    };
+
+};
