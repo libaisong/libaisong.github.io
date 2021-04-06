f550e1949de661771437b6123ac4148a3ecd1bcb
Li Baisong <baisong.li@foxmail.com>
2021-04-06 13:20:38 +0800

    修改items所有页面返回按钮的功能，将链接改为与浏览器返回一样的返回功能

---
+++ js/items.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var backtoprevious = document.querySelector('.item_continue');
+    backtoprevious.onclick = function()
+    {
+        window.history.back();
+    };
+
+};
