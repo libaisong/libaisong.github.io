0a76ba73dd832fd25c5503dc9dfbdd89b25a356c
Li Baisong <baisong.li@foxmail.com>
2021-04-08 01:02:13 +0800

    js和css增加版本号

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-document.writeln("<link rel=\'stylesheet\' href=\'css/normalize.css\'>");
-document.writeln("<link rel=\'stylesheet\' href=\'css/index.css\'>");
-document.writeln("<link rel=\'stylesheet\' href=\'font/style.css\'>");
-document.writeln("<script src=\'js/nav.js\'></script>");
+document.writeln("<link rel=\'stylesheet\' href=\'css/normalize.css?v=20210408\'>");
+document.writeln("<link rel=\'stylesheet\' href=\'css/index.css?v=20210408\'>");
+document.writeln("<link rel=\'stylesheet\' href=\'font/style.css?v=20210408\'>");
+document.writeln("<script src=\'js/nav.js?v=20210408\'></script>");
