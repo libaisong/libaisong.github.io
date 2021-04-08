25a8032396c32b7784591740824c0cd387c10441
Li Baisong <baisong.li@foxmail.com>
2021-04-08 09:44:16 +0800

    修改js和css版本号为20210409

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-document.writeln("<link rel=\'stylesheet\' href=\'css/normalize.css?v=20210408\'>");
-document.writeln("<link rel=\'stylesheet\' href=\'css/index.css?v=20210408\'>");
-document.writeln("<link rel=\'stylesheet\' href=\'font/style.css?v=20210408\'>");
-document.writeln("<script src=\'js/nav.js?v=20210408\'></script>");
+document.writeln("<link rel=\'stylesheet\' href=\'css/normalize.css?v=20210409\'>");
+document.writeln("<link rel=\'stylesheet\' href=\'css/index.css?v=20210409\'>");
+document.writeln("<link rel=\'stylesheet\' href=\'font/style.css?v=20210409\'>");
+document.writeln("<script src=\'js/nav.js?v=20210409\'></script>");
