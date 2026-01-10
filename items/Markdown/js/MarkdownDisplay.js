8140213ce4ed81b99c2a9438a6dde874ceee8edb
Li Baisong <baisong.li@foxmail.com>
2026-01-10 22:49:27 +0800

    整理MarkdownDisplay.js

--- items/Markdown/js/MarkdownDisplay.js
+++ items/Markdown/js/MarkdownDisplay.js
@@ -38,44 +38,21 @@ function MardownDisplay(markdownContent, MarkdownFileName) {
       // 加载mathjax渲染公式
       loadScript("js/mathjax/3.2.0/es5/tex-mml-chtml.js")
         .then(() => {
-          try {
-            markdownContent.innerHTML = markdownText;
-            MathJax.typesetPromise(); // 让 MathJax 重新渲染页面上的数学公式
-            markdownContent.innerHTML = marked.parse(markdownContent.innerHTML);
+          MathjaxRun(markdownContent, markdownText);
 
-            // 加载mermaid渲染图表
-            loadScript("js/mermaid/dist/mermaid.min.js")
-              .then(() => {
-                try {
-                  // 初始化 Mermaid（可选配置）
-                  mermaid.initialize({
-                    theme: 'default',
-                    startOnLoad: false // 禁止自动渲染
-                  });
-                  // 手动渲染 Mermaid 图表
-                  mermaid.run({
-                    querySelector: '.mermaid',
-                    suppressErrors: true
-                  });
-                }
-                catch (error) {
-                  markdownContent.innerHTML = tmp + "<br>图表渲染出错，请刷新页面重试，谢谢~";
-                  tmp = markdownContent.innerHTML;
-                }
-              })
-              .catch(error => {
-                markdownContent.innerHTML = tmp;
-              });
-          }
-          catch (error) {
-            markdownContent.innerHTML = tmp + "<br>公式渲染出错，请刷新页面重试，谢谢~";
-            tmp = markdownContent.innerHTML;
-          }
+          // 加载mermaid渲染图表
+          loadScript("js/mermaid/dist/mermaid.min.js")
+            .then(() => {
+              MermaidRun();
+            })
+            .catch(error => {
+              markdownContent.innerHTML = tmp;
+            });
         })
         .catch(error => {
           markdownContent.innerHTML = tmp;
         });
-        
+
     })
     .catch(error => {
       markdownContent.innerHTML = "加载文件时出错，请刷新页面重试，谢谢~";
@@ -90,4 +67,37 @@ function loadScript(url) {
         script.onerror = reject;
         document.head.append(script);
     });
+}
+
+function MathjaxRun(markdownContent, markdownText) {
+  try {
+    markdownContent.innerHTML = markdownText;
+    MathJax.typesetPromise(); // 让 MathJax 重新渲染页面上的数学公式
+    markdownContent.innerHTML = marked.parse(markdownContent.innerHTML);
+    console.log("rrr")
+  }
+  catch (error) {
+    markdownContent.innerHTML = tmp + "<br>公式渲染出错，请刷新页面重试，谢谢~";
+    tmp = markdownContent.innerHTML;
+    console.log("rrr2")
+  }
+}
+
+function MermaidRun() {
+  try {
+    // 初始化 Mermaid（可选配置）
+    mermaid.initialize({
+      theme: 'default',
+      startOnLoad: false // 禁止自动渲染
+    });
+    // 手动渲染 Mermaid 图表
+    mermaid.run({
+      querySelector: '.mermaid',
+      suppressErrors: true
+    });
+  }
+  catch (error) {
+    markdownContent.innerHTML = tmp + "<br>图表渲染出错，请刷新页面重试，谢谢~";
+    tmp = markdownContent.innerHTML;
+  }
 }
