5dbf3aa2cb169d123a584c05a9afd4d6f6c6a654
Li Baisong <baisong.li@foxmail.com>
2026-01-11 22:48:50 +0800

    添加网页图标

--- js/item_header.js
+++ js/item_header.js
@@ -2,3 +2,7 @@ document.writeln("<link rel=\'stylesheet\' href=\'../css/normalize.css?v=2021042
 document.writeln("<link rel=\'stylesheet\' href=\'../css/index.css?v=20210423\'>");
 document.writeln("<link rel=\'stylesheet\' href=\'../font/style.css?v=20210423\'>");
 document.writeln("<script src=\'../js/nav.js?v=20210423\'></script>");
+document.writeln("<link rel=\'apple-touch-icon\' sizes=\'180x180\' href=\'../favicon/apple-touch-icon.png\'>");
+document.writeln("<link rel=\'icon\' type=\'image/png\' sizes=\'32x32\' href=\'../favicon/favicon-32x32.png\'>");
+document.writeln("<link rel=\'icon\' type=\'image/png\' sizes=\'16x16\' href=\'../favicon/favicon-16x16.png\'>");
+document.writeln("<link rel=\'manifest\' href=\'../favicon/site.webmanifest\'></link>");
